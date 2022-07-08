0425773be0d6776963e23d2d89823d12c9322448
IshmeetSingh06 <singhishmeet16@gmail.com>
2022-07-08 22:09:59 +0530

    axios instance added

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const axiosInstance=axios.create(
-    {baseURL: "https://backend-book-app-kal.herokuapp.com/api"}
+    {baseURL: "https://backend-book-app-kal.herokuapp.com/api/"}
 )
